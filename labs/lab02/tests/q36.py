08bf971b6e443b88ee9e8cda9bb7c66e791fc494
lujiec2020 <lujiec2020@gmail.com>
2025-12-05 23:28:29 -0500

    hw-lab-demo

---
+++ labs/lab02/tests/q36.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+test = {   'name': 'q36',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> baltimore_markets.num_rows == 24\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': ">>> len(list(baltimore_markets.column('city'))) == 24\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
